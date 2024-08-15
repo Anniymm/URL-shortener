a6e7dc4f575b8ad05c268c53df2a6ffcc87808b1
Anniymm <animaisuradze1999@gmail.com>
2024-08-15 16:03:10 +0400

    add features for shortener

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+class URL(models.Model):
+    original_url = models.URLField(max_length=200)
+    short_url = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return f'{self.original_url} -> {self.short_url}'
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = self.generate_short_url()
+        super().save(*args, **kwargs)
+
+    def generate_short_url(self):
+        characters = string.ascii_letters + string.digits
+        short_url = ''.join(random.choice(characters) for _ in range(6))
+        while URL.objects.filter(short_url=short_url).exists():
+            short_url = ''.join(random.choice(characters) for _ in range(6))
+        return short_url
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import URL
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        original_url = request.POST['original_url']
+        url, created = URL.objects.get_or_create(original_url=original_url)
+        return render(request, 'app/home.html', {'short_url': url.short_url})
+    return render(request, 'app/home.html')
+
+def redirect_url(request, short_url):
+    url = get_object_or_404(URL, short_url=short_url)
+    return redirect(url.original_url)
